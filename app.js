d72aa10ffe3dea9c8fa6ecf994aa323347041b59
Plewka Maciej <plewka-maciej@wp.pl>
2024-02-13 20:50:45 +0100

    add FilePath

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const os = require(`os`)
-const {readFileSync,wirteFileSync, writeFileSync}= require('fs')
-// info about current user
-const user = os.userInfo()
-console.log(user)
-const first = readFileSync('./content/first.txt', 'utf8')
-console.log(first)
-writeFileSync(
-    './textS', "eloo"
-)
+// const os = require(`os`)
+// const {readFileSync,wirteFileSync, writeFileSync}= require('fs')
+// // info about current user
+// const user = os.userInfo()
+// console.log(user)
+// const first = readFileSync('./content/first.txt', 'utf8')
+// console.log(first)
+// writeFileSync(
+//     './textS', "eloo"
+// )
+const path = require('path')
+const filePath = path.join('/content','test.txt')
+console.log(filePath)
