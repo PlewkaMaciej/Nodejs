f9bd7bb45bab42d64b35e7fbbc1476288b7b60b4
Plewka Maciej <plewka-maciej@wp.pl>
2024-02-24 17:36:01 +0100

    learning backend

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
-// const os = require(`os`)
-// const {readFileSync,wirteFileSync, writeFileSync}= require('fs')
-// // info about current user
-// const user = os.userInfo()
-// console.log(user)
-// const first = readFileSync('./content/first.txt', 'utf8')
-// console.log(first)
-// writeFileSync(
-//     './textS', "eloo"
-// )
-// const path = require('path')
-// const filePath = path.join('/content','test.txt')
-// console.log(filePath)
-// const base= path.basename(filePath)
-// console.log(base)
-const {readFile, writeFile}=require('fs')
-readFile('./content/first.txt',(err, result)=>{
-    if(err){
-        console.log(err)
-        return
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    if(req.url==='/'){
+        res.end('Welcome to our home page')
     }
-    console.log(result)
+    if(req.url==='/about'){
+        res.end('Welcome to history')
+    }
+res.end(`
+<h1>
+OOOPS
+</h1>
+`)
 })
+server.listen(5000)
